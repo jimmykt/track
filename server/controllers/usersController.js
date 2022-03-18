ba85836ad8f0620ab2482fe160f503edca77dd9a
Ka-Kit Jimmy Tam <90652743+jimmykt@users.noreply.github.com>
2022-03-18 12:10:19 -0400

    email varification

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -4,23 +4,33 @@ const jwt = require("jsonwebtoken");
 
 module.exports.createUser = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
 
-  const newUser = new User({
-    firstName,
-    lastName,
-    email,
-    password: hashedPassword,
-  });
-
-  newUser
-    .save()
-    .then((response) => {
-      res.send(response);
-    })
-    .catch((err) => {
+  User.find({ email: email }, (err, foundUser) => {
+    if (err) {
+      found = false;
       console.log(err);
-    });
+      res.send(err);
+      const hashedPassword = bcrypt.hashSync(password, 10);
+      const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      });
+      newUser
+        .save()
+        .then((response) => {
+          res.send(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      return;
+    } else {
+      found = true;
+      console.log("---found user --new email required");
+    }
+  });
 };
 
 module.exports.loginUser = async (req, res) => {
@@ -32,13 +42,12 @@ module.exports.loginUser = async (req, res) => {
     const isPasswordCorrect = bcrypt.compareSync(password, foundUser.password);
     if (!isPasswordCorrect) return res.status(400).send("Invalid password");
 
-    console.log(foundUser);
-
     const user = {
       firstName: foundUser.firstName,
       lastName: foundUser.lastName,
       email: foundUser.email,
     };
+
     const accessToken = jwt.sign(user, process.env.ACCESS_TOCKEN_SECRET);
 
     res.json({ accessToken });
